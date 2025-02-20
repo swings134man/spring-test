05d283624c9411a3dbe258acf29533c69542a4af
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2025-02-20 16:54:52 +0900

    init: 성능테스트 모듈 생성

---
+++ performance-test/src/main/java/com/lucas/performancetest/PerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.performancetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTestApplication.class, args);
+	}
+
+}
